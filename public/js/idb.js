85a81ac05360305371057611714497c52eee54e1
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-04-15 10:45:20 -0500

    fix indexedDB connection

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,57 +1,52 @@
 // variable to hold db connection
 let db;
 
-// establish a connection to IndexedDB databes called 'budget_tracker' and set to version 1
-const request = indexedDb.open('budget_tracker', 1);
+// establish a connection to IndexedDB database called 'budget_tracker' and set to version 1
+const request = indexedDb.open('budget', 1);
 
 // event to emit if db version changes (v1 to v2)
-request.onupgradeneeded = function (event) {
+request.onupgradeneeded = function (e) {
 
   // saves a reference to the db
-  const db = event.target.result;
+  const db = e.target.result;
 
   // creates an object store (table) called "new_transaction" auto incremented primary key
-  db.createObjectStore('new_transaction', {
+  db.createObjectStore('pending', {
     autoIncrement: true
   });
 };
 
 // upon success
-request.onsuccess = function (event) {
+request.onsuccess = function (e) {
   // when db is successfully create, save refernce to b in global variable
-  db = event.target.result;
+  db = e.target.result;
 
   // check if app is online, if so, run uploadTransaction(). This will send all local data to api
   if (navigator.online) {
-    uploadTransaction();
+    checkDatabase();
   }
 };
 
-request.onerror = function (event) {
-  console.log(event.target.errorCode);
+request.onerror = function (e) {
+  console.log(e.target.errorCode);
 };
 
 function saveRecord(record) {
-
-  const transaction = db.transaction(['new_transaction'], readwrite);
-
-  constbudgetObjectStore = transaction.ObjectStore = transaction.objectStore('new_transaction');
-
-  budgetObjectStore.add(record);
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const store = transaction.objectStore('pending');
+  store.add(record);
 }
 
-// This function executes if attempt to submit new transaction with no internet connection
-function uploadTransaction() {
+function checkDatabase() {
+  const transaction = db.transaction.objectStore('pending');
+  const store = transaction.objectStore('pending');
+  const getAll = store.getAll();
 
-  const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-  const budgetObjectStore = transaction.objectStore('new_transaction');
-
-  const getAll = budgetObjectStore.getAll();
-
+  // This function executes if attempt to submit new transaction with no internet connecti
   getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
-      fetch('/api/transaction', {
+      fetch('/api/transaction/bulk', {
           method: 'POST',
           body: JSON.stringify(getAll.result),
           headers: {
@@ -60,22 +55,19 @@ function uploadTransaction() {
           }
         })
         .then(response => response.json())
-        .then(serverResponse => {
-          if (serverResponse.message) {
-            throw new Error(serverResponse);
-          }
-
-          const transaction = db.transaction(['new_transaction'], 'readwrite');
-
-          const budgetObjectStore = transaction.objectStore('new_transaction');
-
-          budgetObjectStore.clear();
-
-          alert('All saved transactions have been submittedd!');
+        .then(() => {
+          const transaction = db.transaction(['pending'], 'readwrite');
+          const store = transaction.objectStore('pending');
+          store.clear();
         })
         .catch(err => {
           console.log(err);
+
         });
     }
   }
-}
+};
+
+
+// listening for app to come back online
+window.addEventListener('online', checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -6,31 +6,15 @@ const FILES_TO_CACHE = [
   "./css/styles.css",
   "./js/index.js",
   "./manifest.json",
-  "public/icons/icon-72x72.png",
-  "public/icons/icon-96x96.png",
-  "public/icons/icon-128x128.png",
-  "public/icons/icon-144x144.png",
-  "public/icons/icon-152x152.png",
-  "public/icons/icon-192x192.png",
-  "public/icons/icon-384x384.png",
-  "public/icons/icon-512x512.png"
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
 ];
-
-self.addEventListener('fetch', function (e) {
-  console.log('fetchrequest :' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-
-      }
-    })
-  )
-})
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
@@ -39,14 +23,14 @@ self.addEventListener('install', function (e) {
       return cache.addAll(FILES_TO_CACHE)
     })
   )
-})
+});
 
 self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
-      })
+      });
       cacheKeeplist.push(CACHE_NAME);
 
       return Promise.all(keyList.map(function (key, i) {
@@ -57,4 +41,20 @@ self.addEventListener('activate', function (e) {
       }));
     })
   );
+});
+
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request :' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('responding with cache : ' + e.request.url);
+        return request
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+
+      }
+    })
+  )
 });
